7451bd6957572972086cf564558c755ab00de7b5
Sb0y <andrei@bagrintsev.me>
2013-09-10 21:13:53 +0400

    updated hybridauth lib for the twitter auth begin work again

--- engine/libs/hybridauth/Hybrid/Providers/Twitter.php
+++ engine/libs/hybridauth/Hybrid/Providers/Twitter.php
@@ -17,14 +17,53 @@ class Hybrid_Providers_Twitter extends Hybrid_Provider_Model_OAuth1
 	{
 		parent::initialize();
 
-		// Provider api end-points
-		$this->api->api_base_url      = "https://api.twitter.com/1/";
+		// Provider api end-points 
+		$this->api->api_base_url      = "https://api.twitter.com/1.1/";
 		$this->api->authorize_url     = "https://api.twitter.com/oauth/authenticate";
 		$this->api->request_token_url = "https://api.twitter.com/oauth/request_token";
 		$this->api->access_token_url  = "https://api.twitter.com/oauth/access_token";
 
+		if ( isset( $this->config['api_version'] ) && $this->config['api_version'] ){
+			$this->api->api_base_url  = "https://api.twitter.com/{$this->config['api_version']}/";
+		}
+ 
+		if ( isset( $this->config['authorize'] ) && $this->config['authorize'] ){
+			$this->api->authorize_url = "https://api.twitter.com/oauth/authorize";
+		}
+
 		$this->api->curl_auth_header  = false;
 	}
+
+ 	/**
+ 	 * begin login step
+ 	 */
+ 	function loginBegin()
+ 	{
+ 		$tokens = $this->api->requestToken( $this->endpoint );
+ 	
+ 		// request tokens as recived from provider
+ 		$this->request_tokens_raw = $tokens;
+ 	
+ 		// check the last HTTP status code returned
+ 		if ( $this->api->http_code != 200 ){
+ 			throw new Exception( "Authentification failed! {$this->providerId} returned an error. " . $this->errorMessageByStatus( $this->api->http_code ), 5 );
+ 		}
+ 	
+ 		if ( ! isset( $tokens["oauth_token"] ) ){
+ 			throw new Exception( "Authentification failed! {$this->providerId} returned an invalid oauth token.", 5 );
+ 		}
+ 	
+ 		$this->token( "request_token"       , $tokens["oauth_token"] );
+ 		$this->token( "request_token_secret", $tokens["oauth_token_secret"] );
+ 	
+		// redirect the user to the provider authentication url with force_login
+ 		if ( isset( $this->config['force_login'] ) && $this->config['force_login'] ){
+ 			Hybrid_Auth::redirect( $this->api->authorizeUrl( $tokens, array( 'force_login' => true ) ) );
+ 		}
+
+		// else, redirect the user to the provider authentication url
+ 		Hybrid_Auth::redirect( $this->api->authorizeUrl( $tokens ) );
+ 	}
 
 	/**
 	* load the user profile from the IDp api client
@@ -46,22 +85,8 @@ class Hybrid_Providers_Twitter extends Hybrid_Provider_Model_OAuth1
 		$this->user->profile->identifier  = (property_exists($response,'id'))?$response->id:"";
 		$this->user->profile->displayName = (property_exists($response,'screen_name'))?$response->screen_name:"";
 		$this->user->profile->description = (property_exists($response,'description'))?$response->description:"";
-		$this->user->profile->firstName   = (property_exists($response,'name'))?$response->name:"";
-		
-		// WARNING !!!
-		// Check if full sized image is available [Sb0y @ 19.08.2012]
-		if (property_exists($response, "profile_image_url"))
-		{
-			$photoURL = str_replace ("_normal", '', $response->profile_image_url);
-			if (!$ch = curl_init ($photoURL))
-			{
-				$this->user->profile->photoURL = $response->profile_image_url;
-			} else {
-				curl_close ($ch);
-				$this->user->profile->photoURL = $photoURL;
-			}	
-		}
-
+		$this->user->profile->firstName   = (property_exists($response,'name'))?$response->name:""; 
+		$this->user->profile->photoURL    = (property_exists($response,'profile_image_url'))?$response->profile_image_url:"";
 		$this->user->profile->profileURL  = (property_exists($response,'screen_name'))?("http://twitter.com/".$response->screen_name):"";
 		$this->user->profile->webSiteURL  = (property_exists($response,'url'))?$response->url:""; 
 		$this->user->profile->region      = (property_exists($response,'location'))?$response->location:"";
